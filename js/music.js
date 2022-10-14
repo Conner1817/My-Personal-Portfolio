5750b77516e091ab6bf2d1237b402393a98b9f05
Guilherme <zero.freitas2002@gmail.com>
2022-10-14 16:07:56 -0300

    Archives
    Folders

---
+++ js/music.js
@@ -0,0 +1,4 @@
+const music = new Audio('../sounds/OP5.mp3');
+music.play();
+music.loop =true;
+
---
+++ js/music2.js
@@ -0,0 +1,4 @@
+const music = new Audio('../sounds/OP7.mp3');
+music.play();   
+music.loop =true;
+
