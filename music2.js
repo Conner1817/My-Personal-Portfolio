e2cfd5435d154f5402c442194a6a394a4d503970
Guilherme <zero.freitas2002@gmail.com>
2022-10-14 17:16:37 -0300

    Comments JS

--- music2.js
+++ music2.js
@@ -1,4 +1,5 @@
-const music = new Audio('sounds/OP7.mp3');
-music.play();   
-music.loop =true;
+//Rodando a música em LOOP
+const music = new Audio('sounds/OP7.mp3'); //Declarando a música e o caminho
+music.play(); //Executando a música 
+music.loop = true; //Definindo que ela irá rodar em loop
 
