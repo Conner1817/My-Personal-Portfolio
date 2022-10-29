540ed97b5bcb678ca85ac30079b880a9e3206766
Guilherme <zero.freitas2002@gmail.com>
2022-10-29 18:22:59 -0300

    New Page - The Upcoming
    Added Upcoming Page

--- js/music3.js
+++ js/music3.js
@@ -1,10 +1,13 @@
 //Rodando a música em LOOP
 window.onload = function() { //Quando a página abrir e carregar, esta função irá executar a música
-    document.getElementById("OP5").play(); //ID da música a ser executada
+    document.getElementById("JL").play(); //ID da música a ser executada
 }
 
 //CÓDIGO ANTIGO ABAIXO
-/* const music = new Audio('../sounds/OP5.mp3'); //Declarando a música e o caminho
+/* const music = new Audio('../sounds/JL.mp3'); //Declarando a música e o caminho
     music.play(); //Executando a música 
     music.loop = true; //Definindo que ela irá rodar em loop */
+  
+
+
 
--- music2.js
+++ music2.js
@@ -1,5 +1,10 @@
 //Rodando a música em LOOP
-const music = new Audio('sounds/OP7.mp3'); //Declarando a música e o caminho
-music.play(); //Executando a música 
-music.loop = true; //Definindo que ela irá rodar em loop
+window.onload = function() { //Quando a página abrir e carregar, esta função irá executar a música
+    document.getElementById("OP7").play(); //ID da música a ser executada
+}
+
+//CÓDIGO ANTIGO ABAIXO
+/* const music = new Audio('sounds/OP7.mp3'); //Declarando a música e o caminho
+    music.play(); //Executando a música 
+    music.loop = true; //Definindo que ela irá rodar em loop */
 
